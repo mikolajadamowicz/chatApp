5a9e957a7d4f0fe0a0334dfa09eb2a8bfc8ce194
Mikolaj <mikolaj.adamowicz@acaisoft.com>
2020-02-06 15:11:23 +0100

    chore: theme colors added

---
+++ src/config/colors.js
@@ -0,0 +1,7 @@
+export default {
+  primary: {basic: '#9C27B0', dark: '#7B1FA2', light: '#E1BEE7'},
+  accent: '#FFC107',
+  shadow: '#000',
+  text: {primary: '#212121', secondary: '#757575', icons: '#FFFFFF'},
+  divider: '#BDBDBD',
+};
